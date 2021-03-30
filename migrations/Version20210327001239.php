8620e282f871408cfc86b70ac9f941ded4273a99
= <=>
2021-03-30 10:24:17 +0200

    Fix problem with migration

--- migrations/Version20210327001239.php
+++ migrations/Version20210327001239.php
@@ -21,14 +21,12 @@ final class Version20210327001239 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE subscription (id INT AUTO_INCREMENT NOT NULL, currency VARCHAR(255) NOT NULL, min DOUBLE PRECISION NOT NULL, max DOUBLE PRECISION NOT NULL, user_id INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D6497BA2F5EB ON user (api_token)');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('DROP TABLE subscription');
-        $this->addSql('DROP INDEX UNIQ_8D93D6497BA2F5EB ON user');
         $this->addSql('ALTER TABLE user DROP api_token');
     }
 }
--- migrations/Version20210327075342.php
+++ migrations/Version20210327075342.php
@@ -23,8 +23,7 @@ final class Version20210327075342 extends AbstractMigration
         $this->addSql('ALTER TABLE subscription CHANGE user_id user_id INT DEFAULT NULL');
         $this->addSql('ALTER TABLE subscription ADD CONSTRAINT FK_A3C664D3A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
         $this->addSql('CREATE INDEX IDX_A3C664D3A76ED395 ON subscription (user_id)');
-        $this->addSql('DROP INDEX UNIQ_8D93D6497BA2F5EB ON user');
-        $this->addSql('ALTER TABLE user ADD is_confirmed TINYINT(1) NOT NULL, DROP api_token');
+        $this->addSql('ALTER TABLE user ADD is_confirmed TINYINT(1) NOT NULL');
     }
 
     public function down(Schema $schema) : void
@@ -33,7 +32,6 @@ final class Version20210327075342 extends AbstractMigration
         $this->addSql('ALTER TABLE subscription DROP FOREIGN KEY FK_A3C664D3A76ED395');
         $this->addSql('DROP INDEX IDX_A3C664D3A76ED395 ON subscription');
         $this->addSql('ALTER TABLE subscription CHANGE user_id user_id INT NOT NULL');
-        $this->addSql('ALTER TABLE user ADD api_token VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci`, DROP is_confirmed');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D6497BA2F5EB ON user (api_token)');
+        $this->addSql('ALTER TABLE user DROP is_confirmed');
     }
 }
